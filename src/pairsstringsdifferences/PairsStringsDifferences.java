e339e69be81c264589b4ffff6c8e08f696b6c24d
gnexplore <gnexplore@gmail.com>
2018-05-13 14:53:30 +0100

    1st Project upload

---
+++ src/pairsstringsdifferences/PairsStringsDifferences.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pairsstringsdifferences;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Benjamin Chinwe
+ */
+public class PairsStringsDifferences {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        String set1, set2; // variable declaration
+
+        List<Character> diff = new ArrayList<>();
+        Scanner input = new Scanner(System.in); //create Scanner object
+
+        System.out.println("Enter first string set:");
+        set1 = input.nextLine();
+
+        System.out.println("Enter second string set:");
+        set2 = input.nextLine();
+
+        // itreate over the letters in set1
+        for (int i = 0; i < set1.length(); i++) {
+            // itreate over the letters in set2
+
+            //for (int j = 0; j < set2.length(); j++) {
+            char c1 = Character.toLowerCase(set1.charAt(i));
+            char c2 = Character.toLowerCase(set2.charAt(i));
+            // compare char in the two strings
+            if (c1 == c2) {
+                diff.add('.');
+            } else {
+                diff.add('*');
+            }
+
+            //}
+        }
+        System.out.println(set1);
+        System.out.println(set2);
+        for (int i = 0; i < diff.size(); i++) {
+            System.out.print(diff.get(i));
+        }
+
+    }
+
+}
